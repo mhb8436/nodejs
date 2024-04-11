c7f6dc6081da32b507d682c032f571d5deaf1f61
mhb8436 <mhb8436@gmail.com>
2024-04-11 11:14:49 +0900

    imac

---
+++ day03/ext-02/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs')
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const result = fs.readFileSync('test.json', 'utf-8');
+    const data = JSON.parse(result);    
+    
+    res.render('pages/index', {data: data});
+});
+app.listen(3000);
+console.log('Server is listening on port 3000');
